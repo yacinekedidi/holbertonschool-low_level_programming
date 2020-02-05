ff83c3ff17d1d34bb080cabd5a228ff6a0b5cd10
yacinekedidi <kedidiyacine@gmail.com>
2020-02-05 12:03:03 +0000

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
-
+/**
+ * main - our main function
+ * @void: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 0
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
