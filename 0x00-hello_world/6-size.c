71d0f5026cde8a0a2649bc0667de50f4c6e27ad7
yacinekedidi <kedidiyacine@gmail.com>
2020-02-05 12:27:35 +0000

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@
 */
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\n");
-printf("Size of an int: 4 byte(s)\n");
-printf("Size of a long int: 4 byte(s)\n");
-printf("Size of a long long int: 8 byte(s)\n");
-printf("Size of a float: 4 byte(s)\n");
+printf("Size of a char: %d\n", sizeof(char));
+printf("Size of an int: %d\n", sizeof(int));
+printf("Size of a long int: %d\n", sizeof(long int));
+printf("Size of a long long int: %d\n", sizeof(long long int));
+printf("Size of a float: %d\n", sizeof(float);
 return (0);
 }
