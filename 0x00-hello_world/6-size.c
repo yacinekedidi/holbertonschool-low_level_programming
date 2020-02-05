2c40c0a8d295d3c682264e7dc0885070bdd33213
yacinekedidi <kedidiyacine@gmail.com>
2020-02-05 12:18:50 +0000

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
-
+/**
+ * main - prints a couple of lines
+ * @void: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 0
+*/
 int main(void)
 {
-  printf("Size of a char: 1 byte(s)\n");
-  printf("Size of an int: 4 byte(s)\n");
-  printf("Size of a long int: 4 byte(s)\n");
-  printf("Size of a long long int: 8 byte(s)\n");
-  printf("Size of a float: 4 byte(s)\n");
-  return 0;
+printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int: 4 byte(s)\n");
+printf("Size of a long long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)\n");
+return (0);
 }
