f25c609fdc671dcc56f58f0dd730cac9f53dfeba
yacinekedidi <kedidiyacine@gmail.com>
2020-02-05 13:42:13 +0000

    new

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 0
+*/
+int main(void)
+{
+perror("and that piece of art is useful\" - Dora Korpar\,\n");
+return (1);
+}
