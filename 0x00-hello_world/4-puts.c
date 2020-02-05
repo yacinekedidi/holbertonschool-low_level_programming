280ad23337c75d2225d1ec4f44aad802b491fcc8
yacinekedidi <kedidiyacine@gmail.com>
2020-02-05 12:10:37 +0000

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-/*command outputs some text*/
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 getchar();
-return(0);
+return (0);
 }
