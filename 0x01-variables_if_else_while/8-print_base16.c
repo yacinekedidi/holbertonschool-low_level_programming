f257711b36c54000b5028652a47eacfb6d10163f
yacinekedidi <kedidiyacine@gmail.com>
2020-02-06 12:48:30 +0000

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+unsigned char i;
+for (i = 00 ; i <= 15 ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
