4754d887b21ef5cb659fbdcb85301e3d8883925b
yacinekedidi <kedidiyacine@gmail.com>
2020-02-06 12:03:09 +0000

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+if (i != 'q' && i != 'e')
+putchar(i);
+putchar('\n');
+return (0);
+}
