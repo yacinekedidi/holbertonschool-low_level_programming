37ce81b403f852c97856927fa2ae8a0d71af5c83
yacinekedidi <kedidiyacine@gmail.com>
2020-02-06 11:48:30 +0000

    new

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+  char i,j;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+for (j = 'A' ; j <= 'Z' ; j++)
+putchar(j);
+putchar('\n');
+return (0);
+}
