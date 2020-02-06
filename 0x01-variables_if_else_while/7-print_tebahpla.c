f2011877332bff6f27c5d7b301f22cb1f054416f
yacinekedidi <kedidiyacine@gmail.com>
2020-02-06 12:23:56 +0000

    new

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+char i;
+for (i = 'z' ; i >= 'a' ; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
