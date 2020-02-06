c2b4e5a3daf922c6c5a4bc00daa9871afff7540e
yacinekedidi <kedidiyacine@gmail.com>
2020-02-06 13:01:37 +0000

    new

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,12 @@
 */
 int main(void)
 {
-unsigned char i;
-for (i = 00 ; i <= 15 ; i++)
+int i;
+char j;
+for (i = 48 ; i <= 57 ; i++)
 putchar(i);
+for (j = 'a' ; j <= 'f' ; j++)
+putchar(j);
 putchar('\n');
 return (0);
 }
