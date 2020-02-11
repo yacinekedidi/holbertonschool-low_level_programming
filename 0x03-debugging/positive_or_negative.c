b3b0de95029d9cb654ca835b045776231cbb163d
yacinekedidi <kedidiyacine@gmail.com>
2020-02-11 11:37:26 +0000

    new

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,11 +9,11 @@
 (* section header: Section description)*
 * Return: 0
 */
-int main(void)
+int positive_or_negative(int n)
 {
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+ 
+  /* srand(time(0));
+     n = rand() - RAND_MAX / 2; */
   if (n > 0)
     printf("%d is positive\n", n);
   else if (n == 0)
