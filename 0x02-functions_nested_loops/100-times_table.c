6e9c57a40d4573afd8d510862918fcefdbfc4fa3
yacinekedidi <kedidiyacine@gmail.com>
2020-02-11 21:15:24 +0000

    new

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,6 +10,9 @@
 void print_times_table(int n)
 {
 int i, j;
+if (n >= 0 && n <= 15)
+{
+  
 for (i = 0 ; i <= n ; ++i)
 {
 for (j = 0 ; j <= n ; ++j)
@@ -59,6 +62,8 @@ _putchar(' ');
  }
  }
 _putchar('\n');
-}
  }
+ }
+}   
+
 
