3e63b2a5bb8a449cac1f6a1ad2bd3dce526ecb4f
yacinekedidi <kedidiyacine@gmail.com>
2020-02-11 17:55:37 +0000

    new

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,9 +10,9 @@
 int main(void)
 {
 int i, sum = 0;
-for (i = 1 ; i < 1024 ; ++i )
+for (i = 1 ; i < 1024 ; ++i)
 if (i % 5 == 0 || i % 3 == 0)
 sum += i;
-printf("%i\n",sum);
+printf("%i\n", sum);
 return (0);
 }
