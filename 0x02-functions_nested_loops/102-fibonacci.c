27a7ec0f3db04c511410be99cb2d0cae620e3ab3
yacinekedidi <kedidiyacine@gmail.com>
2020-02-11 18:14:19 +0000

    new

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+long int i, numbers[5];
+numbers[0] = 0;
+numbers[1] = 1;
+for (i = 2 ; i < 50 ; ++i)
+numbers[i] = numbers[i - 2] + numbers[i - 1];
+for (i = 0 ; i < 49 ; ++i)   
+printf("%li, ",numbers[i]);
+printf("%li\n",numbers[49]);
+return (0);
+}
