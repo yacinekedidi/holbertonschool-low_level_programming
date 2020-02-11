bbe72eb86d02beed3937baa3cb0d5c3b0a9f35c5
yacinekedidi <kedidiyacine@gmail.com>
2020-02-11 08:47:56 +0000

    new

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 /**
  * add - Short description, single line
- * @a: Description of parameter a
- * @b: Description of parameter b
-(*
+ * @a: Description of parameter x
+(* @b: descripttion
 * Description: Longer description of the function)?
+(* section header: Section description)*
 * Return: Description of the returned value
 */
 int add(int a, int b)
 {
-int x = a + b;
-return (x);
+return (a + b);
 }
