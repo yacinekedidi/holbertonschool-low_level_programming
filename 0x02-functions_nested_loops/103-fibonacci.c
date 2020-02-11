192f48b7f20e419ff199300c92c543985465161d
yacinekedidi <kedidiyacine@gmail.com>
2020-02-11 19:09:06 +0000

    new

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+unsigned long fibo = 0, f1 = 1, f2 = 2, sum = 2;  
+while (fibo <= 4000000)
+{
+fibo = f2 + f1;
+if (fibo % 2 == 0)
+sum += fibo;
+
+f1 = f2;
+f2 = fibo;
+}
+printf("%lu\n", sum);
+return (0);
+}
