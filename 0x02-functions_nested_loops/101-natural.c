41a0dfa65f228d7e55c3ff48e7c39ae328992ac8
yacinekedidi <kedidiyacine@gmail.com>
2020-02-11 17:54:31 +0000

    new

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+ * @void: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+int i, sum = 0;
+for (i = 1 ; i < 1024 ; ++i )
+if (i % 5 == 0 || i % 3 == 0)
+sum += i;
+printf("%i\n",sum);
+return (0);
+}
