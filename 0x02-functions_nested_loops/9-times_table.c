198a26e0fe69c87cfe86bb3e6b4600c545f10e65
yacinekedidi <kedidiyacine@gmail.com>
2020-02-11 08:45:41 +0000

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,40 +1,34 @@
 #include "holberton.h"
-
-
-
-
-
+/**
+ * times_table - Short description, single line
+ * @void: Description of parameter x
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
 void times_table(void)
 {
-  int i,j;
-
-  for (i = 0 ; i <= 9 ; ++i)
-    {
-      
-      for (j = 0 ; j <= 9 ; ++j)
-	{ 
-	  if ( i * j <= 9)
-	    {
-	 _putchar(i*j + '0');
-	 _putchar(',');
-	  _putchar(' ');
-	  _putchar(' ');
-	    }
-	  else
-	    {
-	      _putchar(i*j/10 + '0');
-	      _putchar(i*j%10 + '0');
-	      _putchar(',');
-	      _putchar(' ');
-	      /*     _putchar(' '); */
-
-	    }
-	 
+int i, j;
+for (i = 0 ; i <= 9 ; ++i)
+{
+for (j = 0 ; j <= 9 ; ++j)
+{
+if (i * j <= 9)
+{
+_putchar(i * j + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
 }
-      _putchar('\n');
-    }
-
-
-
-
+else
+{
+_putchar(i * j / 10 + '0');
+_putchar(i * j % 10 + '0');
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
