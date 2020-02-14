388d305010ddbc85b90814ae4723bb58b72282d0
yacinekedidi <kedidiyacine@gmail.com>
2020-02-14 07:22:46 -0800

    a function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+*mul - Adds two integers
+*@a: number 1 to be added
+*@b: number 2 to be added
+*
+*Description: Adds a and b and returns the sum
+*Return: returns the sum of a and b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
