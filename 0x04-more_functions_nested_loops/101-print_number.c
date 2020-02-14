b29de6ebc490d72e7b48478b717471ec0d623c08
yacinekedidi <kedidiyacine@gmail.com>
2020-02-14 14:07:06 -0800

    a function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
 * print_number - Short description, single line
-* @size: Description of parameter size
+* @n: Description of parameter size
 * Return: Description of the returned value
 */
 void print_number(int n)
@@ -17,7 +17,7 @@ while (x > 0)
 {
 x /= 10;
 d++;
-if( d > 1)
+if (d > 1)
 s *= 10;
 }
 while (d != 1)
@@ -26,7 +26,7 @@ y = n / s;
 _putchar(y + '0');
 d--;
 n = n % s;
- s /= 10;
+s /= 10;
 }
 _putchar(n % 10 + '0');
 _putchar('\n');
