0a257360167dd6f2595e72b7859160ee7cdf9a3b
yacinekedidi <kedidiyacine@gmail.com>
2020-02-17 10:02:01 +0100

    first

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Short description, single line
+ * @n: Description of parameter
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
