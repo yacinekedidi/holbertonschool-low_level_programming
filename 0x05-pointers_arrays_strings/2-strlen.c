b55aebc4d247422f260f0d7f8630b85dca0e9461
yacinekedidi <kedidiyacine@gmail.com>
2020-02-17 10:52:06 +0100

    new

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strlen - Short description, single line
+ * @s: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int _strlen(char *s)
+{
+  int i = 0;
+  while ( s[i] != '\0')
+    i++; 
+ return (i);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _puts - Short description, single line
+ * @str: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+void _puts(char *str)
+{
+  int i = 0;
+  while (str[i] != '\0')
+    {
+    _putchar(str[i]);
+    i++;    
+}
+  _putchar('\n');
+}
+
+
+
+
+
