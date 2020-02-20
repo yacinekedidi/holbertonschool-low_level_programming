57e45f2ea2032c4615807db3581a738a6bccbca1
yacinekedidi <kedidiyacine@gmail.com>
2020-02-20 20:28:01 +0100

    new

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,7 +7,7 @@
 */
 char *leet(char *s)
 {
-int i, j;
+int i = 0, j;
 char tab[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 char numbers[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 while (s[i] != '\0')
