83d803a543ee2f540b5f0911d50e118052e7b55a
yacinekedidi <kedidiyacine@gmail.com>
2020-02-20 09:23:41 +0100

    a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * swap_int - Short description, single line
+ * @dest: Description of parameter dest
+(* @src: Description of parameter src
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+char *_strcat(char *dest, char *src)
+{
+char *x = dest;
+int i = 0, j = 0, a = 0;
+while (dest[i] != '\0')
+i++;
+while (src[j] != '\0')
+j++;
+for (a = 0 ; a <= j ; a++)
+dest[a + i] = src[a];
+return (x);
+}
