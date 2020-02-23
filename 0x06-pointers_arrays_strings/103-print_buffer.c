991540ac190411ca957e5782d78fec894c08fa2a
yacinekedidi <kedidiyacine@gmail.com>
2020-02-23 23:03:11 +0100

    new

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -9,10 +9,6 @@
 void print_buffer(char *b, int size)
 {
 int i, j, isprint(int);
-if (size =< 0)
-printf("\n");
-else
-{
 for (i = 0 ; i < size ; i += 10)
 {
 printf("%08x: ", i);
@@ -39,5 +35,7 @@ printf(".");
 }
 printf("\n");
 }
-}
+
+if (size <= 0)
+printf("\n");
 }
