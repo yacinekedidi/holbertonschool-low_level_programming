f8cc7963aa5e745b1e8f71943dc52580cc0a16e0
yacinekedidi <kedidiyacine@gmail.com>
2020-02-29 13:55:02 +0100

    a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
- * _main - Short description, single line
+ * main - Short description, single line
  * @argc: Description of parameter
- * @argv: Description of parameter
+ * @argv: Description of paramete
 * Return: Description of the returned value
 */
 int main(int argc, char *argv[])
 {
-  printf("%s\n",*argv);
-
-  return (argc);
-
+printf("%s\n", *argv);
+return (argc);
 }
