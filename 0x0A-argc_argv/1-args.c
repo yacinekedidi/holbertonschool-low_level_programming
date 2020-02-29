0b55027f1e82aed2633e528279d321092b62137f
yacinekedidi <kedidiyacine@gmail.com>
2020-02-29 14:08:00 +0100

    a program that prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <string.h>
+/**
+ * main - Short description, single line
+ * @argc: Description of parameter
+ * @argv: Description of paramete
+* Return: Description of the returned value
+*/
+int main(int argc, char *argv[])
+{
+if (**argv == '.')
+printf("%i\n", argc);
+return (0);
+}
