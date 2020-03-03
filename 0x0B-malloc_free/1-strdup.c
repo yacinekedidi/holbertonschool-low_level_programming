dbe593c19d807a5971c44a059b0cc1187ee3980e
yacinekedidi <kedidiyacine@gmail.com>
2020-03-03 12:58:51 +0100

    a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,52 +1,29 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
- * _strlen - Short description, single line
- * @s: Description of parameter x
-(*
-* Description: Longer description of the function)?
-(* section header: Section description)*
-* Return: Description of the returned value
-*/
-int _strlen(char *s)
-{
-int i = 0;
-while (s[i] != '\0')
-i++;
-return (i);
-}
-/**
- * _strcpy - Short description, single line
- * @dest: Description of parameter x
-(* @src: Desription
-* Description: Longer description of the function)?
-(* section header: Section description)*
-* Return: Description of the returned value
-*/
-char *_strcpy(char *dest, char *src)
-{
-int i = 0, j;
-while (src[i] != '\0')
-i++;
-for (j = 0 ; j <= i ; j++)
-dest[j] = src[j];
-return (dest);
-}
-/**
- * _strdup - Short description, single line
- * @str: Description of parameter
-* Return: Description of the returned value
-*/
+ *_strdup - description
+ *@str: description
+ *Return: description
+ */
 char *_strdup(char *str)
 {
-size_t l = _strlen(str) + 1;
-char *x;
+int i = 0, j = 0, l = 0;
+char *s;
 if (str == NULL)
 return (NULL);
-x =  malloc(l * sizeof(char));
-if (x == NULL)
+while (str[i] != '\0')
+{
+l++;
+i++;
+}
+s = malloc(sizeof(char) * (l + 1));
+if (s == NULL)
 return (NULL);
-return (_strcpy(x, str));
+while (j < l + 1)
+{
+s[j] = str[j];
+j++;
+}
+s[j] = '\0';
+return (s);
 }
