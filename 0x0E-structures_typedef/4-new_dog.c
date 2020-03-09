77927557a92dd7fe50c985e54c2b060fc4c7bb2a
yacinekedidi <kedidiyacine@gmail.com>
2020-03-09 14:16:48 +0100

    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,31 @@
 #include <stdlib.h>
 #include "dog.h"
+/**
+ *_strdup - description
+ *@str: description
+ *Return: description
+ */
+char *_strdup(char *str)
+{
+int i = 0, j = 0;
+char *s;
+if (str == NULL)
+return (NULL);
+while (str[i] != '\0')
+{
+i++;
+}
+s = malloc(sizeof(char) * (i + 1));
+if (s == NULL)
+return (NULL);
+while (j < i + 1)
+{
+s[j] = str[j];
+j++;
+}
+s[j] = '\0';
+return (s);
+}
 
 /**
  * new_dog - check the code for Holberton School students.
@@ -11,14 +37,19 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *x;
-       
+	char *name1;
+	char *owner1;
+
 	if (owner == NULL || name == NULL)
 		return (NULL);
+	name1 = _strdup(name);
+	owner1 = _strdup(owner);
+
 	x = malloc(sizeof(dog_t));
 	if (x == NULL)
 		return(NULL);
-	x->name = name;
+	x->name = name1;
 	x->age = age;
-	x->owner = owner;
+	x->owner = owner1;
 return (x);
 }
