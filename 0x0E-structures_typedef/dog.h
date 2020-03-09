6fe04ab06eec7ddf23a695bf7f607e09ddfd1393
yacinekedidi <kedidiyacine@gmail.com>
2020-03-09 13:23:56 +0100

    a new type struct dog with the following elements:
    name, type = char *
    age, type = float
    owner, type = char *

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 /**
  *struct dog - check the code for Holberton School students.
  *@name: pointer to a char
@@ -7,7 +7,7 @@
  *@owner: string
  * Description: Always.
  */
-typedef struct dog
+struct dog
 {char *name;
 float age;
 char *owner;
