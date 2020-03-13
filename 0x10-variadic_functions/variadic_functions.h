c334a7adfea9977dfc61909cb3fd1d14ee0d4169
yacinekedidi <kedidiyacine@gmail.com>
2020-03-13 20:03:09 +0100

    header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef _VAR_FUN_
 #define _VAR_FUN_
 #include <stdarg.h>
+/**
+ * struct f - Struct f
+ *
+ * @fo: The format
+ * @x: The function associated
+ */
+typedef struct f
+{
+char *fo;
+void (*x)();
+} f_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
