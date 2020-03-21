b73815c41aee0f2c5ff035dbe9d13210600d486f
yacinekedidi <kedidiyacine@gmail.com>
2020-03-21 23:33:14 +0100

    fix

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -11,4 +11,6 @@ char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+void print_number(int n);
+int _putchar(char c);
 #endif
