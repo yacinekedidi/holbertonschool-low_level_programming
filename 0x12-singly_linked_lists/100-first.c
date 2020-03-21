989f4b25329e4ecde96b17f0fb7fec13727128a7
yacinekedidi <kedidiyacine@gmail.com>
2020-03-21 18:03:43 +0100

    f

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 
-void __attribute__((constructor)) first();
+void __attribute__((constructor)) first(void);
+
 /**
  * first - prints a message before the main function.
  *
@@ -10,11 +11,6 @@ void __attribute__((constructor)) first();
 
 void first(void)
 {
-
-
-	printf("You're beat! and yet, you must allow,\nI
-bore my house upon my back!\n");
-
-
-
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
