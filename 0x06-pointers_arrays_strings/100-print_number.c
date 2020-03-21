75ebad2bea5f20c5cf6aa9bf532c4dab28a6bdcd
yacinekedidi <kedidiyacine@gmail.com>
2020-03-21 23:29:43 +0100

    add

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+* print_number - Short description, single line
+* @n: Description of parameter size
+* Return: Description of the returned value
+*/
+
+void print_number(int n)
+{
+	int i = 0, j, div = 1, tmp, tmpn = n;
+
+	if (n < 0)
+	{
+		if (n == -2147483648)
+			n += 1;
+		_putchar('-');
+		n = -n;
+	}
+	tmp = n;
+	while (tmp / 10 > 0)
+	{
+		tmp /= 10;
+		i++;
+	}
+
+	for (j = 0 ; j < i ; j++)
+	div *= 10;
+
+	while (div > 0)
+	{
+		if (tmpn == -2147483648 && n < 10)
+			n += 1;
+	_putchar(n / div + '0');
+	n %= div;
+	div /= 10;
+	}
+}
