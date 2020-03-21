81f5cb57d8c4024a4751f4bcf542f7aa71d49133
yacinekedidi <kedidiyacine@gmail.com>
2020-03-21 17:59:54 +0100

    n

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 	for (i = 0 ; h != NULL ; i++)
 	{
 		if (h->str != NULL)
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] %s\n", "(nil)");
 		h = h->next;
