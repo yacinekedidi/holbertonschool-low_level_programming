cb626e1eedb74a8a3ccfce437c89244f2653ec47
yacinekedidi <kedidiyacine@gmail.com>
2020-03-21 19:35:06 +0100

    fix

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,28 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Short description, single line
- * @void: Description of parameter x
-(* a blank line
-* Description: Longer description of the function)?
-(* section header: Section description)*
-* Return: Description of the returned value
-*/
+ * main - main func.
+ * @void : nothing
+ * Return: 0.
+ */
+
 int main(void)
 {
-int i, j;
-for (i = 48 ; i <= 57 ; i++)
-{
-for(j = 48 ; j <= 57 ; j++)
-{
-putchar(i);
-putchar(j);
-if(i+j != 114)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	int i, j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+
+	for (j = 0 ; j < 10 ; j++)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+		if (i != 9 || j != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
