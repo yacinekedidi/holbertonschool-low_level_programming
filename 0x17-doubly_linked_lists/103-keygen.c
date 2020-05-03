593f140b7abdc930d50759a06d37369a9c8c53f6
yacinekedidi <kedidiyacine@gmail.com>
2020-05-03 23:54:35 +0100

    crackme5

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+
+
+
+
+
+
+
+
+
+  return (0);
+}
