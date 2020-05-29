700f37e9f6d80a2200f9ccfded11276d4f2517a1
yacinekedidi <kedidiyacine@gmail.com>
2020-05-29 14:39:42 +0100

    C

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - check the code for Holberton School students.
+ * @ht: hashtable
+ * Return: Always EXIT_SUCCESS.
+ */
+
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *tmp;
+
+	if (!ht)
+		return;
+
+
+	printf("{");
+	for (; i < ht->size ; i++)
+	{
+	tmp = ht->array[i];
+		while (tmp)
+		{
+			if (i != (ht->size) - 1 && !tmp)
+				printf("'%s': '%s',", tmp->key, tmp->value);
+			else
+				printf("'%s': '%s'", tmp->key, tmp->value);
+		 tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+
+
+	return;
+
+
+}
