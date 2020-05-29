83920204b487ab13fae41659562a9bf5ddcf716e
yacinekedidi <kedidiyacine@gmail.com>
2020-05-29 19:40:24 +0100

    C

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -94,11 +94,13 @@ char *val;
 char *shash_table_get(const shash_table_t *ht, const char *key)
 {
 	shash_node_t *h = NULL;
+	unsigned long int i;
 
 	if (!ht || !key)
 		return (NULL);
-	/*i = key_index((const unsigned char *)key, ht->size);*/
-
+	i = key_index((const unsigned char *)key, ht->size);
+	if (i >= ht->size)
+		return (NULL);
 
 	h = ht->shead;
 	while (h)
