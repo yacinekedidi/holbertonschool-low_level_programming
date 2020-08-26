2b368a7f2c00ef12be3d9835e720572137f5620c
yacinekedidi <kedidiyacine@gmail.com>
2020-08-26 12:31:57 +0100

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,8 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
+/* advanced tasks */
+int jump_search(int *array, size_t size, int value);
+
 
 #endif
